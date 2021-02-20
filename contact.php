2fce028cf7e2cf52c19e0670c167ce70c41b1376
Maverick <maverick.champi@gmail.com>
2021-02-19 21:22:13 -0500

    section listo

---
+++ contact.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="portfolio,portafolio,Maverick Champi">
+    <meta name="description" content="Portafolio de Maverick Champi">
+    <meta name="author" content="Maverick Champi">
+    <title>Portfolio | Maverick</title>
+    <link rel="stylesheet" href="scss/portfolio.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/icon.png">
+</head>
+
+<body>
+    <header class="header">
+        
+
+        
+        <nav class="navigation">
+            <div class="logo">
+                <a href="index.php"> <img src="images/icon.png" alt="Logo"> </a>
+                <!-- <a href="#" class="burger" id="burger"><i class="fa fa-bars"></i></a> -->
+            </div>
+            <ol class="menu" id="menu">
+                <li>
+                    <a class="link" href="index.php"><i class="fas fa-home"></i><span>Home</span></a>
+                </li>
+                <li>
+                    <a class="link" href="about.php"><i class="fas fa-user"></i><span>About</span></a>
+                </li>
+                <li>
+                    <a class="link" href="works.php"><i class="fas fa-briefcase"></i><span>Works</span></a>
+                </li>
+                <li>
+                    <a class="link active" href="contact.php"><i class="fas fa-envelope"></i><span>Contact</span></a>
+                </li>
+            </ol>
+            <ol class="social">
+                <li>
+                    <a class="link" href="https://www.linkedin.com/in/maverickchampi/"><i class="fab fa-linkedin-in"></i></a>
+                </li>
+                <li>
+                    <a class="link" href="https://github.com/maverickchampi"><i class="fab fa-github"></i></a>
+                </li>
+                <li>
+                    <a class="link" href="https://wa.link/abw60e"><i class="fab fa-whatsapp"></i></a>
+                </li>
+            </ol>
+        </nav>
+    </header>
+
+    <script src="https://kit.fontawesome.com/905fa6d085.js" crossorigin="anonymous"></script>
+    <script src="js/portfolio.js"></script>
+    <!-- <script src="js/block.js"></script> -->
+</body>
+
+</html>
